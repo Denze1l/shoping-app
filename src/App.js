f4c440e61739b7980a1dfb88c879f6218971d822
Denze1l <dvornikovd.o.1998@gmail.com>
2022-04-14 10:21:47 +0300

    add selector

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <Auth />
-      {/* <Layout /> */}
-    </div>
-  );
+  const userLogged = useSelector((state) => state.isLogged);
+  console.log(userLogged);
+  console.log(userLogged);
+  return <div className="App">{userLogged ? <Layout /> : <Auth />}</div>;
 }
 
 export default App;
