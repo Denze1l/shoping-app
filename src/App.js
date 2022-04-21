0ae5224c9058ad7f560d15fa63c113199c4f1949
Denze1l <dvornikovd.o.1998@gmail.com>
2022-04-21 17:16:43 +0300

    add notification

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch;
+  const notification = useSelector((state) => state.ui.Notification);
   const cart = useSelector((state) => state.addItem);
   const userLogged = useSelector((state) => state.login.isLogged);
   useEffect(() => {
+    dispatch(
+      uiActions.showNotification({
+        open: true,
+        message: "Sending request",
+        type: "warning",
+      })
+    );
     const sendRequest = async () => {
       const res = await fetch(
         "https://shoping-app-926f6-default-rtdb.firebaseio.com/cartItems.json",
@@ -18,13 +28,28 @@ function App() {
         }
       );
       const data = await res.json();
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sent successfully",
+          type: "success",
+        })
+      );
     };
-    sendRequest();
+    sendRequest().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending request failed",
+          type: "error",
+        })
+      );
+    });
   }, [cart]);
 
   return (
     <div className="App">
-      <Notification type="success" message={"Is it work?"} />
+      <Notification type={notification.type} message={notification.message} />
       {userLogged ? <Layout /> : <Auth />}
     </div>
   );
